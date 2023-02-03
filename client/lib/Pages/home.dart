ef7a527c4f82731482a820324cba1e460f1eaa10
zakespy <satyam.ms@somaiya.edu>
2023-02-03 22:42:06 +0530

    Home page rough layout created

--- client/lib/Pages/home.dart
+++ client/lib/Pages/home.dart
@@ -26,29 +26,94 @@ class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(children: <Widget>[
+          Text("Widget 1"),
+          Text("Widget 2"),
+          Text("Widget 3"),
+        ]),
+      ),
       appBar: AppBar(
-        elevation: 0,
-        toolbarHeight: 200,
-        backgroundColor: Colors.amber,
-        title: const Padding(padding: EdgeInsets.fromLTRB(10, 20, 10, 20),child: Text("Wednesday offer",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),),),
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-            // gradient: LinearGradient(colors: colors),
-            // image: DecorationImage(image: NetworkImage("https://images.pexels.com/photos/5560763/pexels-photo-5560763.jpeg"),alignment: Alignment.centerRight),
-            image: DecorationImage(image: AssetImage('assets/dosa.png'),alignment: Alignment.centerRight,scale: 0.01),
+        title: Text("appbar"),
+        actions: [Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(5,5,10,5),
+              child: Container(
+                height: 40,
+                width: 40,
+                // color: Colors.black,
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+                child: IconButton(icon: Icon(Icons.person_2_rounded),onPressed: ()=>{},),
+              ),
+            )
+          ],
+        )],
+        // elevation: 0,
+        // toolbarHeight: 200,
+        // backgroundColor: Colors.amber,
+        // title: const Padding(padding: EdgeInsets.fromLTRB(10, 20, 10, 20),child: Text("Wednesday offer",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),),),
+        // flexibleSpace: Container(
+        //   decoration: const BoxDecoration(
+        //     // gradient: LinearGradient(colors: colors),
+        //     // image: DecorationImage(image: NetworkImage("https://images.pexels.com/photos/5560763/pexels-photo-5560763.jpeg"),alignment: Alignment.centerRight),
+        //     image: DecorationImage(image: AssetImage('assets/dosa.png'),alignment: Alignment.centerRight,scale: 0.01),
+        //   ),
+        // ),
+      ),
+      body:SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              height: 200,
+              width: 400,
+              color: Colors.amber,
+              child: Center(
+                child: 
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const <Widget>[
+                  SizedBox(width:5),
+                  Padding(padding: EdgeInsets.fromLTRB(10, 20, 10, 20),child: Text("Wednesday offer",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),),),
+                  SizedBox(width: 25),
+                  Image(image: AssetImage("assets/dosa.png"),alignment: Alignment.center,height: 150,width: 150,)
+                ]),
+              ),
+            ),
           ),
-        ),
-      ),
-      body:Container(
-      child: Center(
-        child: ElevatedButton(
-          child: Text("Logout"),
-          onPressed: () {
-            logout();
-          },
-        ),
-      ),
-    ),
+          Container(
+            height: 50,
+            // color: Colors.blue,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+              TextButton(onPressed: ()=>{}, child: const Text("Menu",style: TextStyle(color: Colors.black, fontSize: 15,fontWeight: FontWeight.w500),)),
+              TextButton(onPressed: ()=>{}, child: const Text("Today's Special",style: TextStyle(color: Colors.black, fontSize: 15,fontWeight: FontWeight.w500),)),
+              TextButton(onPressed: ()=>{}, child: const Text("Offer",style: TextStyle(color: Colors.black, fontSize: 15,fontWeight: FontWeight.w500),)),
+            ],)
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10,0,10,10),
+            child: Container(
+              height: 650,
+              width: 400,
+              color: Colors.red[200],
+            ),
+          ),
+        ],),
+      )
+    //   Container(
+    //   child: Center(
+    //     child: ElevatedButton(
+    //       child: Text("Logout"),
+    //       onPressed: () {
+    //         logout();
+    //       },
+    //     ),
+    //   ),
+    // ),
     );
   }
 }
