502812a761ff72c5d68af86a23d2e79f4a92b8bc
RakshitPatel910 <86058505+RakshitPatel910@users.noreply.github.com>
2023-03-03 23:35:18 +0530

    createOrder controller created

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -1,6 +1,21 @@
+import Razorpay from "razorpay";
 
 export const createOrder = async (req, res) => {
 
+    var instance = new Razorpay( { key_id: 'rzp_test_rk34q3MXaI4tBi', key_secret: 'T5RC8mNXy9yJANYD6QLdEUPH' } );
+    var options = {
+        amount: 100,
+        currency: "INR",
+        receipt: "rcpt_id_1",
+    };
+
     
+    try {
+        let order = await instance.orders.create(options);
+
+        res.status(201).json( { success: true, order: order } );
+    } catch (error) {
+        res.status(500).json( { message: error.message } );
+    }
 
 };
--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,5 +1,9 @@
 import express from "express";
 
+import { createOrder } from "../controllers/paymentController.js";
+
 const router = express.Router();
+
+router.post("/createOrder", createOrder);
 
 export default router;
