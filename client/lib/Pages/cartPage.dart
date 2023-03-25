7a5ea0d9d5763ebd97a8c3101d95a776f679f128
zakespy <satyam.ms@somaiya.edu>
2023-03-25 10:42:06 +0530

    ..

--- client/lib/Pages/cartPage.dart
+++ client/lib/Pages/cartPage.dart
@@ -167,69 +167,7 @@ class _CartState extends State<CartPage> {
     }
   }
 
-  List<Item> products = [
-    Item(
-        name: 'Apple',
-        unit: 'Kg',
-        price: 20,
-        image: 'assets/dosa.png',
-        quantity: 0),
-    Item(
-        name: 'Mango',
-        unit: 'Doz',
-        price: 30,
-        image: 'assets/dosa.png',
-        quantity: 0),
-    Item(
-        name: 'Banana',
-        unit: 'Doz',
-        price: 10,
-        image: 'assets/dosa.png',
-        quantity: 0),
-    Item(
-        name: 'Grapes',
-        unit: 'Kg',
-        price: 8,
-        image: 'assets/dosa.png',
-        quantity: 0),
-    Item(
-        name: 'Water Melon',
-        unit: 'Kg',
-        price: 25,
-        image: 'assets/dosa.png',
-        quantity: 0),
-    Item(
-        name: 'Kiwi',
-        unit: 'Pc',
-        price: 40,
-        image: 'assets/dosa.png',
-        quantity: 0),
-    Item(
-        name: 'Orange',
-        unit: 'Doz',
-        price: 15,
-        image: 'assets/dosa.png',
-        quantity: 0),
-    Item(
-        name: 'Peach',
-        unit: 'Pc',
-        price: 8,
-        image: 'assets/dosa.png',
-        quantity: 0),
-    Item(
-        name: 'Strawberry',
-        unit: 'Box',
-        price: 12,
-        image: 'assets/dosa.png',
-        quantity: 0),
-    Item(
-        name: 'Fruit Basket',
-        unit: 'Kg',
-        price: 55,
-        image: 'assets/dosa.png',
-        quantity: 0),
-  ];
-
+  
   @override
   Widget build(BuildContext context) {
     int calcTotal() {
@@ -549,3 +487,68 @@ class _CartState extends State<CartPage> {
         ));
   }
 }
+
+
+
+// List<Item> products = [
+//     Item(
+//         name: 'Apple',
+//         unit: 'Kg',
+//         price: 20,
+//         image: 'assets/dosa.png',
+//         quantity: 0),
+//     Item(
+//         name: 'Mango',
+//         unit: 'Doz',
+//         price: 30,
+//         image: 'assets/dosa.png',
+//         quantity: 0),
+//     Item(
+//         name: 'Banana',
+//         unit: 'Doz',
+//         price: 10,
+//         image: 'assets/dosa.png',
+//         quantity: 0),
+//     Item(
+//         name: 'Grapes',
+//         unit: 'Kg',
+//         price: 8,
+//         image: 'assets/dosa.png',
+//         quantity: 0),
+//     Item(
+//         name: 'Water Melon',
+//         unit: 'Kg',
+//         price: 25,
+//         image: 'assets/dosa.png',
+//         quantity: 0),
+//     Item(
+//         name: 'Kiwi',
+//         unit: 'Pc',
+//         price: 40,
+//         image: 'assets/dosa.png',
+//         quantity: 0),
+//     Item(
+//         name: 'Orange',
+//         unit: 'Doz',
+//         price: 15,
+//         image: 'assets/dosa.png',
+//         quantity: 0),
+//     Item(
+//         name: 'Peach',
+//         unit: 'Pc',
+//         price: 8,
+//         image: 'assets/dosa.png',
+//         quantity: 0),
+//     Item(
+//         name: 'Strawberry',
+//         unit: 'Box',
+//         price: 12,
+//         image: 'assets/dosa.png',
+//         quantity: 0),
+//     Item(
+//         name: 'Fruit Basket',
+//         unit: 'Kg',
+//         price: 55,
+//         image: 'assets/dosa.png',
+//         quantity: 0),
+//   ];
