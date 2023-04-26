4d6d6645337b3648b37fe84a8d7c440bdcf11760
code108 <110485188+code108pp@users.noreply.github.com>
2023-04-26 20:49:26 +0530

    Ordered Card completed

--- dashboard/dashbd/src/components/OrderCard/Ordercard.js
+++ dashboard/dashbd/src/components/OrderCard/Ordercard.js
@@ -1,35 +1,61 @@
 import React from "react";
 import "./OrderCard.css";
-
+import item from "./Somosa.jpeg";
 
 export default function OrderCard(){
 
-    var seconds = 0
-    setInterval(upTimer, 1000);
-    function upTimer() {
-        ++seconds;
-        var hour = Math.floor(seconds / 3600);
-        var minute = Math.floor((seconds - hour * 3600) / 60);
-        var updSecond = seconds - (hour * 3600 + minute * 60);
-        document.getElementById("countup").innerHTML = hour + ":" + minute + ":" + updSecond;
+    var minutesLabel = document.getElementById("minutes");
+    var secondsLabel = document.getElementById("seconds");
+    var totalSeconds = 0;
+    setInterval(setTime, 1000);
+  
+    function setTime() {
+      ++totalSeconds;
+      secondsLabel.innerHTML = pad(totalSeconds % 60);
+      minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
     }
+  
+    function pad(val) {
+      var valString = val + "";
+      if (valString.length < 2) {
+        return "0" + valString;
+      } else {
+        return valString;
+      }
+    }
+    
+    return(    
+    <div className="card">
 
-    return(
-        <>
-        <div className="card">
-            <div className="details">
-                <p className="token">Token No- 12</p>
-                <p className="foodname">Samosa</p>
-                <p className="price">100 Rupees</p>
-            </div>
-            <div className="timer">
-            <h2 id= "countup">00:00:00</h2>
-            </div>
-            {/* <div className="delete"> */}
-            <button><span>Order Completed</span></button>
-            {/* </div> */}
-        </div>  
-        </>        
+        <div class="left">
+        <img src={item} alt="hii" className="pic"/>
+        </div>
+
+        <div className="right">
+
+        <div className="detail">
+            <p className="token">Token 33</p>
+            <p className="name">Samosa</p>
+            <p className="price">Rs. 20</p>
+        </div>
+        
+        <div className="timer">
+            <label id="minutes">00</label>
+            <label id="colon">:</label>
+            <label id="seconds">00</label>
+        </div>
+
+        <div className="delete">
+        <button>
+            <span class="transition"></span>
+            <span class="gradient"></span>
+            <span class="label">Order Completed</span>
+        </button>
+        </div>
+
+        </div>
+    </div>
+  
     )
 }
 
